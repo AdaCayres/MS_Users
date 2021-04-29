c8c2543cbf0bfdf0e2505b6612923fb9f2156af3
Ada <adacayres@outlook.com>
2021-04-29 09:26:00 -0300

    docker & mysql integration

--- src/main/java/br/com/ada/livraria/MS_Users/Model/UserTypes/Adiministrador.java
+++
@@ -1,9 +0,0 @@
-package br.com.ada.livraria.MS_Users.Model.UserTypes;
-
-public class Adiministrador extends Usuario{
-
-    public Adiministrador(String nick, String nome, String senha) {
-        super(nick, nome, senha);
-    }
-
-}
--- src/main/java/br/com/ada/livraria/MS_Users/Model/UserTypes/Vendedor.java
+++
@@ -1,7 +0,0 @@
-package br.com.ada.livraria.MS_Users.Model.UserTypes;
-
-public class Vendedor extends Usuario{
-    public Vendedor(String nick, String nome, String senha){
-        super(nick, nome, senha);
-    }
-}
